b216a3f54eebcc239f1c359dc1f6f68b3bbf78ac
Darthone <dm661@live.com>
2015-12-12 15:59:32 -0500

    fixed leaf scaling

--- Awesome-Game/Assets/Scripts/Leaf.cs
+++ Awesome-Game/Assets/Scripts/Leaf.cs
@@ -3,7 +3,20 @@ using System.Collections;
 
 public class Leaf : MonoBehaviour {
 	// Update is called once per frame
+    float x, y;
+    void Start() {
+        Vector3 scale = this.transform.localScale;
+        if (scale.x < 0)
+            x = -0.001f;
+        else
+            x = 0.001f;
+        if (scale.y < 0)
+            y = -0.001f;
+        else
+            y = 0.001f;
+    }
+
 	void FixedUpdate () {
-        this.transform.localScale += new Vector3(0.001f, 0.001f, 0);	
+        this.transform.localScale += new Vector3(x, y, 0);	
 	}
 }
