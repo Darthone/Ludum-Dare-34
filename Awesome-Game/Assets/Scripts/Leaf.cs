d2b933c7c65965d7eb4cb0a9193af7785ccd6c7e
Darthone <dm661@live.com>
2015-12-12 13:42:47 -0500

    added leaf prefab

---
+++ Awesome-Game/Assets/Scripts/Leaf.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leaf : MonoBehaviour {
+	// Update is called once per frame
+	void FixedUpdate () {
+        this.transform.localScale += new Vector3(0.001f, 0.001f, 0);	
+	}
+}
