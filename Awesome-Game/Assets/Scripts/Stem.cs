ff34354b3fe09c856445bdfdcfbb010e38240e0c
Darthone <dm661@live.com>
2015-12-12 13:43:11 -0500

    Added growth to stems

--- Awesome-Game/Assets/Scripts/Stem.cs
+++ Awesome-Game/Assets/Scripts/Stem.cs
@@ -4,19 +4,23 @@ using System.Collections;
 public class Stem : MonoBehaviour {
 
     PlayerController pc;
-
     bool Growing = true;
 
 	// Use this for initialization
 	void Start () {
         this.pc = GameController.control.pc;
-	
 	}
 	
+    public void StopGrowing(){
+        Growing = false;
+    }
+
 	// Update is called once per frame
 	void FixedUpdate () {
         if (Growing) {
             this.transform.localScale += new Vector3(0, pc.GetGrowthSpeed() * 0.05f, 0);
+        } else {
+            this.transform.localScale += new Vector3(0.001f, 0f, 0);
         }
 	}
 }
