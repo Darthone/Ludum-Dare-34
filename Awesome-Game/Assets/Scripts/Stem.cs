918ffcf8a3c84a206eed0d2031ff3d2b248563eb
Darthone <dm661@live.com>
2015-12-12 16:00:00 -0500

    removed magic number

--- Awesome-Game/Assets/Scripts/Stem.cs
+++ Awesome-Game/Assets/Scripts/Stem.cs
@@ -5,7 +5,7 @@ public class Stem : MonoBehaviour {
 
     PlayerController pc;
     bool Growing = true;
-
+    
 	// Use this for initialization
 	void Start () {
         this.pc = GameController.control.pc;
@@ -18,7 +18,7 @@ public class Stem : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         if (Growing) {
-            this.transform.localScale += new Vector3(0, pc.GetGrowthSpeed() * 0.05f, 0);
+            this.transform.localScale += new Vector3(0, pc.GetGrowthSpeed(), 0);
         } else {
             this.transform.localScale += new Vector3(0.001f, 0f, 0);
         }
