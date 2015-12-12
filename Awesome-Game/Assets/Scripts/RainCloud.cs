b1ed3a957066d96a18b9144f346e965eb529abf7
Darthone <dm661@live.com>
2015-12-12 17:00:22 -0500

    spawner for creating rain

---
+++ Awesome-Game/Assets/Scripts/RainCloud.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainCloud : MonoBehaviour {
+    System.Random rng = new System.Random();
+    public float RainDelay;
+    public float SpawnTime;
+    public float SpawnRange; // horizontal line
+    public GameObject RainPrefab;
+    public bool IsRaining = true;
+
+
+    IEnumerator DelayRain(float delay) {
+        yield return new WaitForSeconds(delay);
+    }
+
+    IEnumerator DelayDrops() {
+        // TODO get random number
+        yield return new WaitForSeconds(SpawnTime);
+        if (IsRaining) {
+            CreateDrop();
+        }
+        StartCoroutine(DelayDrops());
+    }
+
+    void CreateDrop() {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	    StartCoroutine(DelayRain(RainDelay))
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    
+	}
+}
