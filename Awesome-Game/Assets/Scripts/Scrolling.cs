800d6a50a73908ebc6759461229149f2776bb6a6
Darthone <dm661@live.com>
2015-12-14 21:07:20 -0500

    parallax scrolling

---
+++ Awesome-Game/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrolling : MonoBehaviour {
+    Renderer R;
+    Vector3 delta;
+    void Start(){
+        this.R = this.GetComponent<Renderer>();
+        delta = Camera.main.transform.position;
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+        delta = delta - Camera.main.transform.position;
+        R.material.mainTextureOffset -= (Vector2)delta * 0.02f;
+        delta = Camera.main.transform.position; 
+	}
+}
